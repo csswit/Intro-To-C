f301b18cc74257861373a1227abbe23ad2159228
Shawn Toubeau <shawntoubeau@gmail.com>
2018-02-18 16:32:30 -0500

    Add files via upload

---
+++ paint/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float price, height, width, surfaceArea;
+    int cans;
+
+    printf("Enter the price of a individual 1 gallon paint can\n");
+    scanf("%f", &price);
+    printf("Enter the dimensions of the surface in feet\n");
+    scanf("%f %f", &height, &width);
+
+    if (price < 0 || height < 0 || width < 0)
+    {
+        printf("Error: Please use positive values\n");
+    }
+
+    surfaceArea = height * width * 4;
+    cans = ceil((int)surfaceArea/400) + 1;
+
+    printf("The number of paint cans needed are %d for a total of $%.2f\n", cans, price*cans);
+    return 0;
+}
